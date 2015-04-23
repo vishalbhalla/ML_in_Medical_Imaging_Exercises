c8f1f00713dfdb0c689ac1f8cc3933cb37078f75
vishalbhalla <vishal.a.bhalla@gmail.com>
2015-04-23 23:41:55 +0200

    Exercise 1 - Changes to randomly generate odd size 'a' of patches or kernels.

--- Exercise 1/Exercise1.m
+++ Exercise 1/Exercise1.m
@@ -20,11 +20,19 @@ imshow(I);
 %  The value of such a feature xk(i, j) is a linear combination of the intensities within the neighbourhood of (i, j).
  
 % Extraction of standard features at each pixel from the given sample image.
-filters = {'straight derivatives', 'diagonal derivatives'};
-side =5;
-sigma = 2.5;
+filters = {'straight derivatives', 'diagonal derivatives', 'mean'};
 
-[X] = standard_filters(I, filters, side, sigma);
+% Generate a random number between 1 to 10
+p = randi(10);
+
+% The size a of patches or kernels must always be an odd number a = 2p+1.
+% The centre of the patch/kernel is then clearly defined as the pixel (r+1;r+1).
+side = 2*p +1; 
+
+% Randomly generate value for sigma.
+sigma = randi(5)/4;
+
+%[X] = standard_filters(I, filters, side, sigma);
 
 
 %% =========== Part 2: Features based on integral images =============
@@ -36,30 +44,41 @@ sigma = 2.5;
 % Call the function integral_image
 Ii = integral_image(I);
 
+% Displaying the Integral Image.
+imshow(Ii);
 
 % =========== Part 2 b: Mean Patch =============
 % Call the function mean_patch
-X2b = mean_patch(I, a);
+%X2b = mean_patch(Ii, side);
 
 
 % =========== Part 2 c: Mean Features =============
 % Call the function mean_features
-X2c = mean_features(I, a);
+X2c = mean_features(Ii, side);
 
 
 % =========== Part 2 d: Local Binary Patterns =============
 % Call the function lbp
-X2d = lbp(I, a);
+X2d = lbp(Ii, side);
 
 
 % =========== Part 2 e: Long Range Features =============
 % We propose to compute now the same kind of features but on longer range.
 
 % (i) Call the function long_range_offset
-X2e1 = long_range_offset(I, a, w);
+% Offset vector
+w = [2,3];
+
+X2e1 = long_range_offset(Ii, side, w);
 
 % (ii) Call the function long_range_two_offsets
-X2e2 = long_range_two_offsets(I, a, w1, w2);
+% First offset vector
+w1 = [2,3];
+
+% Second offset vector
+w2 = [4,6];
+
+X2e2 = long_range_two_offsets(Ii, side, w1, w2);
 
 
 
