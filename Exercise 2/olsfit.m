869d2b47ed17880adce45cf24f96a0f557f0e926
vishalbhalla <vishal.a.bhalla@gmail.com>
2015-05-01 01:25:56 +0200

    1a. Changes in the olsfit.m function to remove the Intercept term.

--- Exercise 2/olsfit.m
+++ Exercise 2/olsfit.m
@@ -1,14 +1,12 @@
-function [Coeff] = olsfit(X, y) 
-% Create a function olsfit which takes a matrix X of R(n X m) of samples and a vector y of R(n X 1) of outcomes for each sample.
-% The function should return the ordinary least squares (OLS) estimate of the coefficients Bˆ (including the intercept).
+function [Coeff] = olsfit(X, Y) 
+% Create a function olsfit which takes as input parameters 
+%  a matrix X of R(n X m) of samples and 
+%  a vector Y of R(n X 1) of outcomes for each sample.
+% Return the ordinary least squares (OLS) estimate of the coefficients Bˆ (including the intercept).
 
 Coeff = 0;
-[n,m] = size(X);
-
-% Add intercept term to X
-X = [ones(n, 1) X];
 
 %Definition (Ordinary Least Squares Estimate)
-Coeff = inv(X'*X)* X' * y;
+Coeff = inv(X'*X)* X' * Y;
 
 end
