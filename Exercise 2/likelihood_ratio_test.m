928189d60a9a0cae791c370935eef0c8320ffea7
vishalbhalla <vishal.a.bhalla@gmail.com>
2015-05-13 23:01:46 +0200

    Implemented likelihood-ratio test to calculate p-value & Test Statistic D.

--- Exercise 2/likelihood_ratio_test.m
+++ Exercise 2/likelihood_ratio_test.m
@@ -1,10 +1,25 @@
-function likelihood_ratio_test()
-% Create a model which considers multiple features by starting with the null model and adding one additional feature at a time. 
-% To determine which feature to add, use the p-value as returned by the likelihood-ratio test.
-% Extended models with one additional feature, where
-%  the p-value is greater than 0.05, should not be considered.
-%  In each step choose the model with the smallest p-value.
-% Continue until all features have been selected or the model cannot be improved significantly any more.
+function [pValue, D] = likelihood_ratio_test(fullModel, reducedModel)
+%% The function likelihood_ratio_test implements the likelihood-ratio test which takes as input parameters
+%  full model and the reduced model.
+%  It applies the log-likelihood over them.
+%  It returns the p-value and the test statistic D of the likelihood-ratio test.
+
+% Apply log-likelihood over each of the models.
+modelF = sigmoid(fullModel);
+modelR = sigmoid(reducedModel);
+
+% Calculate the deviance for both old and new models based on the B parameters.
+logLikelihoodmodelFull = -2 * sum(log(model_new));
+logLikelihoodmodelReduced = -2 * sum(log(model_old));
+
+% Calculate the Test Statistic of the likelihood-ratio test.
+D = -2 * log(logLikelihoodmodelFull/logLikelihoodmodelReduced);
+
+% Degree of Freedom (df) is equal to the difference in the number of features considered.
+df = size(fullModel,2) - size(reducedModel,2);
+
+% Calculate the p-value.
+pValue = gammainc(D/2, df/2, 'upper');
 
 
 end
